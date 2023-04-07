36bdbd4ae038bfa4940dcb29bd99be42354b86c1
cagataysadic <71948079+cagataysadic@users.noreply.github.com>
2023-04-07 18:04:24 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const path = require('path')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 10000;
